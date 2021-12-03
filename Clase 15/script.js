38addd73c8d4a9d0b3c953568425fc9a96f85b05
solsansserri <solsansserri@gmail.com>
2021-12-03 19:05:01 -0300

    actualizaciones

---
+++ Clase 15/script.js
@@ -0,0 +1,8 @@
+function mostrarMensaje() {
+    var mensajes =document.getElementById("datosIngresados").value;
+    document.querySelector("h1").textContent = mensajes;
+}
+
+var formulario =document.querySelector("#formulario");
+formulario.addEventListener("input", mostrarMensaje);
+

4f41fd0d9c8206b5e44ab17dac2b97975b578236
solsansserri <solsansserri@gmail.com>
2021-12-06 16:32:39 -0300

    actualizaciones

--- Javascript Mi Formulario/script.js
+++ Javascript Mi Formulario/script.js
@@ -1,7 +1,7 @@
 function mostrarMensaje() {
-    var mensajes =document.getElementById("datosIngresados").value;
+    var mensajes =document.getElementById("Apellidoynombre").value;
     document.querySelector("h1").textContent = mensajes;
 }
 
-var formulario =document.querySelector("#formulario");
+var formulario =document.querySelector("#formulario1");
 formulario.addEventListener("input", mostrarMensaje);
